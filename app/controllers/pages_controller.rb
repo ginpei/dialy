0d71674c9023131937b3f34e9ab2b615e8fa2c7d
Takanashi Ginpei <ginpei+github@gmail.com>
2011-09-30 01:22:42 +0900

    Revert "Revert 7c8e6ae35b4c993c4bd16afc8bfb25536e304820^..HEAD"
    This reverts commit 4cbb63e9cf834e00a7857f48a37fc1b8c9e4a39b.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   # GET /pages
   # GET /pages.json
   def index
-    @pages = Page.all
+    @pages = Page.order("date desc").all
 
     respond_to do |format|
       format.html # index.html.erb
