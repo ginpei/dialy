6e00171ac7d150dbfd3633e3af27c879bd423574
Takanashi Ginpei <ginpei+github@gmail.com>
2011-10-15 05:39:40 +0900

    Marge reverted unit tests.

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  test "event array" do
+  test "get_for_index" do
+    pages = Page.get_for_index
+    assert_equal 2, pages[0].id
+    assert_equal 1, pages[1].id
+  end
+
+  test "get_yesterday" do
+    yesterday = Page.get_yesterday pages(:two)
+    assert_equal 1, yesterday.id
+  end
+
+  test "get_diff" do
+    assert_equal -1200, pages(:two).get_diff(:cash, pages(:one))
+    assert_equal -400, pages(:two).get_diff(:suica, pages(:one))
+  end
+
+  test "get_diff_text" do
+    assert_equal "(-1,200)", pages(:two).get_diff_text(:cash, pages(:one))
+    assert_equal "(+300)", pages(:three).get_diff_text(:cash, pages(:two))
+    assert_equal "(0)", pages(:three).get_diff_text(:suica, pages(:two))
+    assert_equal "", pages(:three).get_diff_text(:suica, nil)
+  end
+
+  test "events array" do
     assert_equal pages(:one).events_array.size, 2
     assert_equal pages(:two).events_array.size, 0
   end
